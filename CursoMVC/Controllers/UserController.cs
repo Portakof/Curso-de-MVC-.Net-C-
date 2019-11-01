4a3ff41d0f84eac0f65f7a2fcf3276918b2868e3
Portakof <elektrakof@gmail.com>
2019-10-31 23:27:46 -0500

    05 Curso de MVC .Net C#  CRUD, Validaciones, Agregar con Entity Framework

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CursoMVC.Models;
 using CursoMVC.Models.TableViewModels;
+using CursoMVC.Models.ViewModels;
 
 namespace CursoMVC.Controllers
 {
@@ -30,6 +31,40 @@ namespace CursoMVC.Controllers
 
 
             return View(lst);   //Se envia a la vista los datos obtenidos de la tabla "user" cargados en un modelo de tipo "UserTableViewModel"
+        }
+
+        [HttpGet]   //Esta etiqueta obliga a que el metodo solo entre en funcionamiento por "get"
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        // "[HttpPost]" se usa para realizar una petición a un servidor enviando información, sea para insertar o consultar algo y esperar
+        // una respuesta luego de finalizada la operación recibir algún tipo de mensaje o información.
+        [HttpPost]  //Esta etiqueta obliga a que el metodo solo entre en funcionamiento por "post"
+        public ActionResult Add(UserViewModels model)
+        {
+            //De esta manera se valida o se ejecuta los "DataAnnotations" colocados en "UserViewModels.cs"            
+            if (!ModelState.IsValid)    //se valida los "DataAnnotations"
+            {
+                return View(model);//si no se cumple los "DataAnnotations" se devuelve la vista con el modelo para que se corrijan o se llenen los campos de ser necesario
+            }
+
+            using (var db = new cursomvcEntities())     //se crea el objeto "db" con la instancia para porder manejar la base de datos "cursomvc"
+            {
+                user oUser = new user();    //"user" es un modelo creado por el Entity frameword que maneja la tabla "user" de la base de datos "cursomvc"
+                oUser.idState = 1;          //Se llenan los valores con los recibidos de la vista
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+                oUser.password = model.Password;
+
+                db.user.Add(oUser);     //se agrega el objeto oUser que es la representacion del "Entities Framework"
+
+                db.SaveChanges();   //se guardan los cambios para agregar el usuario nuevo en la base datos
+            }
+
+            //Si todo esta bien se redirecciona al "UserUserController" en el cual se mostrara el nuevo usuario en la vista
+            return Redirect(Url.Content("~/User"));     
         }
     }
 }
---
+++ CursoMVC/Models/ViewModels/UserViewModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models.ViewModels
+{
+    //Esta clase se crea porque son los unicos datos que el cliente podra crear y que seran ingresados a la base datos
+    //Estos parametros se llenan por el formulario creado en "Register.cshtml"
+    //"Register.cshtml" es la vista creada para que el usuario pueda ingresar los datos de registro
+
+    public class UserViewModels
+    {
+        [Required]                      //Hace que el campo o textbox se obligatorio ingresarle datos
+        [EmailAddress]                  //valida que el texto incluido se de tipo mail osea "@mail.com"
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracteres",MinimumLength = 1)]//restringe o permite solo cierta cantidad de caracteres "(100)"
+        [Display(Name = "Correo electronico")]//ES el nombre que mostrara la vista
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]   //De esta maneja cuando se envia el dato no es visible, como si estubiera encriptado
+        [Display(Name = "Contraseña")]
+        public string Password  { get; set; }
+
+        [Display(Name = "Confirmar contraseña")]
+        [DataType(DataType.Password)]
+        //realiza una comparacion entre dos campos dentro de los parantesis debe ir el nombre de la variable a comparar
+        [Compare("Password", ErrorMessage ="Las contraseñas no son correctas")]              //asi se compara "Password" y "ConfirmPassword"
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+}
