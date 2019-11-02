fea9d73311638d15229146ea0f48c42f4f52cb32
Portakof <elektrakof@gmail.com>
2019-11-02 12:44:30 -0500

    07 Curso de MVC .Net C# Cerrar sesión

---
+++ CursoMVC/Controllers/CerrarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    //Este conntroler se crea solo para menejar el cierre de la sesion, esto para no crear un conflicto con el filtro 
+    //creado en "Filters/VerifySesion.cs"
+    public class CerrarController : Controller
+    {
+        
+        public ActionResult Logoff()
+        {
+            Session["User"] = null;     //Se cierra la sesion
+            return RedirectToAction("Index", "Access");     //Se redirecciona para poder iniciar nuevo login o inicio de sesion
+        }
+    }
+}
