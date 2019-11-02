68725e8f72dfff0da62585f4ea7ac412e251aad9
Portakof <elektrakof@gmail.com>
2019-11-01 22:42:55 -0500

    06 Curso de MVC .Net C#  CRUD, Editar y Eliminar con Entity Framework

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -20,7 +20,7 @@ namespace CursoMVC.Controllers
             {
                 lst = (from d in db.user        //Se accedera a la tabla "user" de la base datos
                        where d.idState == 1     //Se Toman solo los registros que coincidan con "1" en el campo "idState"
-                       orderby d.email          //Se ordenan en base al campo "email"
+                       orderby d.id          //Se ordenan en base al campo "email"
                        select new UserTableViewModel    //Se llena el objeto en base al modelo
                        {
                            Email = d.email,
@@ -33,13 +33,17 @@ namespace CursoMVC.Controllers
             return View(lst);   //Se envia a la vista los datos obtenidos de la tabla "user" cargados en un modelo de tipo "UserTableViewModel"
         }
 
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
         [HttpGet]   //Esta etiqueta obliga a que el metodo solo entre en funcionamiento por "get"
         public ActionResult Add()
         {
             return View();
         }
 
-        // "[HttpPost]" se usa para realizar una petición a un servidor enviando información, sea para insertar o consultar algo y esperar
+ //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+         // "[HttpPost]" se usa para realizar una petición a un servidor enviando información, sea para insertar o consultar algo y esperar
         // una respuesta luego de finalizada la operación recibir algún tipo de mensaje o información.
         [HttpPost]  //Esta etiqueta obliga a que el metodo solo entre en funcionamiento por "post"
         public ActionResult Add(UserViewModels model)
@@ -64,7 +68,94 @@ namespace CursoMVC.Controllers
             }
 
             //Si todo esta bien se redirecciona al "UserUserController" en el cual se mostrara el nuevo usuario en la vista
-            return Redirect(Url.Content("~/User"));     
+            return Redirect(Url.Content("~/User/"));     
+        }
+
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        //Si no tiene una etiqueta [Http] significa que el metodo va a funcionar por default por [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            EditUserViewModels model = new EditUserViewModels();
+
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(Id);     //"Find" trae un objeto de la base datos con ese "Id"
+                model.Edad = (int)oUser.edad;
+                model.Email = oUser.email;
+                model.Id = oUser.id;
+            }
+
+            return View(model);
+        }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        [HttpPost]
+        public ActionResult Edit(EditUserViewModels model)
+        {
+
+            //De esta manera se valida o se ejecuta los "DataAnnotations" colocados en "EditUserViewModels.cs"            
+            if (!ModelState.IsValid)    //se valida los "DataAnnotations"
+            {
+                return View(model);//si no se cumple los "DataAnnotations" se devuelve la vista con el modelo para que se corrijan o se llenen los campos de ser necesario
+            }
+
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(model.Id);     //"Find" trae un objeto de la base datos con ese "Id"
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+
+                //Entra solo si se escribio un nuevo "Password" de lo contrario se deja el ya existente en la base datos
+                if (model.Password != null && model.Password.Trim() != "")
+                {
+                    oUser.password = model.Password;
+                }
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;    //De esta manera se le dice al Entity Framework que el regitro se modifico
+                db.SaveChanges(); //Se actualiza en la base datos
+            }
+
+            //Si todo esta bien se redirecciona al "UserUserController" en el cual se mostrara el nuevo usuario en la vista
+            return Redirect(Url.Content("~/User/"));
+        }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(Id);     //"Find" trae un objeto de la base datos con ese "Id"
+
+                //En el metodo "Index" al tomar los datos de la base datos, solo se tienen en cuenta los registros
+                //que esten en "uno(1)" osea "d.idState == 1" al realizar el cambio por el numero "tres(3)" cuando
+                //se vuelva a tomar los datos lo deja por fuera
+                oUser.idState = 3;  
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;    //De esta manera se le dice al Entity Framework que el regitro se modifico
+                db.SaveChanges(); //Se actualiza en la base datos
+            }
+
+            //"Content" en vez de regresar una vista regresa un contenido en este caso un string "1"
+            return Content("1");
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CursoMVC/Models/ViewModels/UserViewModels.cs
+++ CursoMVC/Models/ViewModels/UserViewModels.cs
@@ -32,4 +32,29 @@ namespace CursoMVC.Models.ViewModels
         [Required]
         public int Edad { get; set; }
     }
+
+    public class EditUserViewModels
+    {
+        public int Id { get; set; }
+
+        [Required]                      //Hace que el campo o textbox se obligatorio ingresarle datos
+        [EmailAddress]                  //valida que el texto incluido se de tipo mail osea "@mail.com"
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 1)]//restringe o permite solo cierta cantidad de caracteres "(100)"
+        [Display(Name = "Correo electronico")]//ES el nombre que mostrara la vista
+        public string Email { get; set; }
+
+        //Se quita la etiqueta "[Required]" porque en la vista si no se escribe un nuevo "Password" se deja el ya existente en la base datos
+        [DataType(DataType.Password)]   //De esta maneja cuando se envia el dato no es visible, como si estubiera encriptado
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmar contraseña")]
+        [DataType(DataType.Password)]
+        //realiza una comparacion entre dos campos dentro de los parantesis debe ir el nombre de la variable a comparar
+        [Compare("Password", ErrorMessage = "Las contraseñas no son correctas")]              //asi se compara "Password" y "ConfirmPassword"
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
 }
